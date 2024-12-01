4894d02e9e00500d5855ceb6318a899ceeacbaa9
dwxtien <hoangductien0004@gmail.com>
2024-12-02 00:53:42 +0700

    Signed-off-by:dwxtien <hoangductien0004@gmail.com>

---
+++ src/main/java/com/mycompany/ajp_nhom8/AJP_Nhom8.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ajp_nhom8;
+/**
+ *
+ * @author nguyentoan
+ */
+public class AJP_Nhom8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
